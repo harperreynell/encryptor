fd281bc63f2b4c3021cf3ddee8c0485218962a2d
harperreynell <sentinel.whoami@gmail.com>
2024-08-09 10:47:03 +0000

    Updating file chosing

--- fileList.go
+++ fileList.go
@@ -25,6 +25,9 @@ func fileList(root string) []string {
 			if info.IsDir() && path == "/sys" {
 				return filepath.SkipDir
 			}
+			if info.IsDir() && path == "/swap" {
+				return filepath.SkipDir
+			}
 
 			if info != nil {
 				List = append(List, path)
