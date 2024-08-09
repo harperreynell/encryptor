d5af30261a036164b83f91c3b64fab0a2b84d1fb
harperreynell <sentinel.whoami@gmail.com>
2024-08-09 10:38:19 +0000

    Updating file chosing

--- fileList.go
+++ fileList.go
@@ -1,16 +1,21 @@
 package main
 
 import (
-	"path/filepath"
 	"os"
+	"path/filepath"
 )
 
 func fileList(root string) []string {
-	List := []string {}
+	List := []string{}
 	_ = filepath.Walk(root,
 		func(path string, info os.FileInfo, err error) error {
 			if err != nil {
 				return nil
+			}
+
+			// Skip the /dev folder
+			if info.IsDir() && path == "/dev" {
+				return filepath.SkipDir
 			}
 
 			if info != nil {
@@ -18,6 +23,6 @@ func fileList(root string) []string {
 			}
 			return nil
 		})
-	
+
 	return List
 }
